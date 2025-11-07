9725d7b930c69db7a26d353031ca04bd3d00f7d3
AjafaM <149159094+theo-picar1@users.noreply.github.com>
2025-11-07 16:51:52 +0000

    build(project): add graphQL dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,4 +1,7 @@
 plugins {
+    id("com.apollographql.apollo") version "4.3.3"
+    id("kotlin-kapt")
+    id("com.google.dagger.hilt.android")
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
@@ -41,7 +44,23 @@ android {
     }
 }
 
+apollo{
+    service("service"){
+        packageName.set("com.example.kotlinqraft") //your package name
+        introspection {
+            endpointUrl.set("https://countries.trevorblades.com/graphql") //replace with your endpoint
+            schemaFile.set(file("src/main/graphql/com/graphql/schema.graphqls")) //replace with the schema file location
+        }
+    }
+}
+
 dependencies {
+    implementation("com.apollographql.apollo:apollo-runtime:4.3.3")
+    implementation("com.squareup.okhttp3:okhttp:1")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.3.0")
+    implementation("com.google.dagger:hilt-android:2.57.2")
+    kapt("com.google.dagger:hilt-compiler:2.57.2")
+    implementation("androidx.fragment:fragment-ktx:1.8.9")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
